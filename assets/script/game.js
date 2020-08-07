acb4155193261c387c91d1300c14d1ecb1c5d186
lyons <guoyang@nplusgroup.com>
2020-08-07 18:53:24 +0800

    动态加载tiledmap地图资源

--- assets/script/game.js
+++ assets/script/game.js
@@ -28,6 +28,7 @@ cc.Class({
         gravity: cc.v2(0, 0), // 系统默认
         mapNode: cc.Node,
         dialogNode: cc.Node,
+        loadNode: cc.Node,
         smog_enabled: true
     },
 
@@ -50,45 +51,92 @@ cc.Class({
         // 打开碰撞设置
         cc.director.getCollisionManager().enabled = true
         cc.director.getCollisionManager().enabledDebugDraw = this.is_debug
+
+        // 显示 loading 层
+        this.loadNode.active = true
+
+        // 地图名称数组
+        let mapNameArr = [
+            ['00000', '01000', '00000'],
+            ['00010', '11110', '00100'],
+            ['00000', '10000', '00000']
+        ]
+        this.initMap(mapNameArr)
+    },
+    // 根据地图名字数组生成地图
+    initMap (mapNameArr) {
+        let mapSt = null
+        let loadCnt = 0
+
+        for (let i = 0; i < mapNameArr.length; i++) {
+            for (let j = 0; j < mapNameArr[i].length; j++) {
+                let mapName = mapNameArr[i][j]
+                if (!mapName || mapName === '00000') { continue }
+
+                if (!mapSt) {
+                    mapSt = { i, j }
+                }
+
+                loadCnt++
+
+                cc.loader.loadRes(`map/${mapName}`, cc.TiledMapAsset, (err, assets) => {
+                    let node = new cc.Node()
+                    let map = node.addComponent(cc.TiledMap)
+                    node.anchorX = node.anchorY = 0  // 设置地图锚点为（0，0）
+                    node.x = (j - mapSt.j) * 384
+                    node.y = -(i - mapSt.i) * 384
+
+                    map.tmxAsset = assets
+                    node.parent = this.mapNode
+                    this.initMapNode(node)
+
+                    if (--loadCnt == 0) {
+                        // 隐藏 loading 层
+                        this.loadNode.active = false
+                    }
+                })
+            }
+        }
     },
 
-    start () {
-        for (let mapNode of this.mapNode.children) { // 遍历所有地图节点，绘制碰撞墙体
-            let tiledMap = mapNode.getComponent(cc.TiledMap)
-            let tiledSize = tiledMap.getTileSize()
-            let layer = tiledMap.getLayer('wall')
-            let layerSize = layer.getLayerSize()
-            let smogLayer = tiledMap.getLayer('smog')
-            smogLayer.node.active = this.smog_enabled  // 打开战争迷雾
+    initMapNode (mapNode) {
+        let tiledMap = mapNode.getComponent(cc.TiledMap)
+        let tiledSize = tiledMap.getTileSize()
+        let layer = tiledMap.getLayer('wall')
+        let layerSize = layer.getLayerSize()
+        let smogLayer = tiledMap.getLayer('smog')
+        smogLayer.node.active = this.smog_enabled  // 打开战争迷雾
 
-            for (let i = 0; i < layerSize.width; i++) {
-                // 水平方向的块数
-                for (let j = 0; j < layerSize.height; j++) {
-                    // 垂直方向的块数
-                    let tiled = layer.getTiledTileAt(i, j, true)
-                    if (tiled.gid != 0) {
-                        tiled.node.group = 'wall'
-                        let body = tiled.node.addComponent(cc.RigidBody)
-                        body.type = cc.RigidBodyType.Static
-                        let collider = tiled.node.addComponent(cc.PhysicsBoxCollider)
-                        collider.offset = cc.v2(tiledSize.width / 2, tiledSize.height / 2)
-                        collider.size = tiledSize
-                        collider.apply()
-                    }
+        for (let i = 0; i < layerSize.width; i++) {
+            // 水平方向的块数
+            for (let j = 0; j < layerSize.height; j++) {
+                // 垂直方向的块数
+                let tiled = layer.getTiledTileAt(i, j, true)
+                if (tiled.gid != 0) {
+                    tiled.node.group = 'wall'
+                    let body = tiled.node.addComponent(cc.RigidBody)
+                    body.type = cc.RigidBodyType.Static
+                    let collider = tiled.node.addComponent(cc.PhysicsBoxCollider)
+                    collider.offset = cc.v2(tiledSize.width / 2, tiledSize.height / 2)
+                    collider.size = tiledSize
+                    collider.apply()
+                }
 
-                    // 给迷雾增加碰撞刚体
-                    tiled = smogLayer.getTiledTileAt(i, j, true)
-                    if (tiled.gid != 0) {
-                        tiled.node.group = 'smog'
-                        let collider = tiled.addComponent(cc.BoxCollider)
-                        collider.offset = cc.v2(tiledSize.width / 2, tiledSize.height / 2)
-                        collider.size = tiledSize
-                    }
+                // 给迷雾增加碰撞刚体
+                tiled = smogLayer.getTiledTileAt(i, j, true)
+                if (tiled.gid != 0) {
+                    tiled.node.group = 'smog'
+                    let collider = tiled.addComponent(cc.BoxCollider)
+                    collider.offset = cc.v2(tiledSize.width / 2, tiledSize.height / 2)
+                    collider.size = tiledSize
                 }
             }
-
         }
-
+    },
+    start () {
+        for (let mapNode of this.mapNode.children) { // 遍历所有地图节点，绘制碰撞墙体
+            this.initMapNode(mapNode)
+        }
 
         // 在对话框上层初始化对话
         // this.dialog = this.dialogNode.getComponent('dialog')
